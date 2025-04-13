b989af5bcc5bc8353a46e808cf43452e946edd9f
spaceAngel <spaceangel@cyberdeck.cz>
2025-04-13 11:18:33 +0200

    dummy closing appointmen modal on configm

--- resources/views/doctor/patients/@compontents/actions.blade.php
+++ resources/views/doctor/patients/@compontents/actions.blade.php
@@ -44,7 +44,7 @@
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zrušit</button>
-				<button type="button" class="btn btn-primary">Objednat pacienta</button>
+				<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Objednat pacienta</button>
 			</div>
 		</div>
 	</div>
